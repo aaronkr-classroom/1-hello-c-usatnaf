84da5066d80047c7ffef53f82d88587ec883808d
usatnaf <bubbylyr0115@naver.com>
2025-09-09 16:55:12 +0900

    u

---
+++ Project1/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int age = 20;
+	char grade = 'A';
+	char name[] = "yerim";
+
+	printf("helllooooo~\n");
+	printf("My name is %s", name);
+	printf("I'm %d years old\n", age);
+	printf("\tI want to a %c grade", grade);
+	return 0;
+}
